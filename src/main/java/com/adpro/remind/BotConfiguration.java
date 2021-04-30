4005955f918bdfd3edac4f1e1a37947d0332dc5a
GerradND <Gerrad1106@gmail.com>
2021-04-30 23:23:03 +0700

    commit 2: Menambahkan beberapa class tapi masih belum bisa dirun

---
+++ src/main/java/com/adpro/remind/BotConfiguration.java
@@ -0,0 +1,33 @@
+package com.adpro.remind;
+
+import com.adpro.remind.events.EventListener;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.Event;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+@Configuration
+public class BotConfiguration {
+
+    @Value("${token}")
+    private String token;
+
+    @Bean
+    public GatewayDiscordClient gatewayDiscordClient() {
+        return DiscordClientBuilder.create(token)
+                .build()
+                .login()
+                .block();
+    }
+}
