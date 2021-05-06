3367340f42b867d7ee1edd48f798f8ab875c4327
Timothy Efraim Hotasi <timothyefraim35@gmail.com>
2021-05-06 22:38:24 +0700

    changes on Schedule attributes

--- src/main/java/com/adpro/remind/model/Schedule.java
+++ src/main/java/com/adpro/remind/model/Schedule.java
@@ -1,81 +1,37 @@
 package com.adpro.remind.model;
 
+import lombok.Data;
 import javax.persistence.*;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "schedule")
+@Data
 public class Schedule {
     @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer idSchedule;
 
-    @Column(name = "title")
+    @Column(name = "title", nullable = false)
     private String title;
 
     @Column(name = "day")
     private String day;
 
     @Column(name = "start_time")
-    private LocalDateTime startTime;
+    private LocalTime startTime;
 
     @Column(name = "end_time")
-    private LocalDateTime endTime;
+    private LocalTime endTime;
 
-    @Column(name = "desc", columnDefinition = "text")
+    @Column(name = "description", columnDefinition = "text")
     private String description;
 
     // implement ManyToOne User
-
-    public Schedule(String title, String day, LocalDateTime startTime, LocalDateTime endTime, String description){
+    public Schedule(String title, String day, LocalTime startTime, LocalTime endTime, String description){
         this.title = title;
         this.day = day;
         this.startTime = startTime;
         this.endTime = endTime;
-        this.description = description;
-
-    }
-
-    public Integer getIdSchedule() {
-        return idSchedule;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public LocalDateTime getStartTime() {
-        return startTime;
-    }
-
-    public LocalDateTime getEndTime() {
-        return endTime;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setIdSchedule(Integer idSchedule) {
-        this.idSchedule = idSchedule;
-    }
-
-    public void setDay(String day) {
-        this.day = day;
-    }
-
-    public void setStartTime(LocalDateTime startTime) {
-        this.startTime = startTime;
-    }
-
-    public void setEndTime(LocalDateTime endTime) {
-        this.endTime = endTime;
-    }
-
-    public void setDescription(String description) {
         this.description = description;
     }
 }
