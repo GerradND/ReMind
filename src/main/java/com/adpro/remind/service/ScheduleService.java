1d9a37bcc0a12c5ec0a3db6b5f5157e90b36a27c
Timothy Efraim Hotasi <timothyefraim35@gmail.com>
2021-05-18 10:45:20 +0700

    Implements schedule feature (not finished yet)

---
+++ src/main/java/com/adpro/remind/service/ScheduleService.java
@@ -0,0 +1,18 @@
+package com.adpro.remind.service;
+
+import com.adpro.remind.model.Schedule;
+import org.springframework.stereotype.Component;
+
+public interface ScheduleService {
+    Schedule createSchedule(Schedule schedule);
+
+    Schedule getScheduleByID(Integer idSchedule);
+
+    Schedule updateSchedule(Integer idSchedule, Schedule schedule);
+
+    void deleteSchedule(Integer idSchedule);
+
+    Iterable<Schedule> getScheduleByDay(String day);
+
+    Iterable<Schedule> getListSchedule();
+}
---
+++ src/main/java/com/adpro/remind/service/ScheduleServiceImpl.java
@@ -0,0 +1,53 @@
+package com.adpro.remind.service;
+
+import com.adpro.remind.model.Schedule;
+import com.adpro.remind.repository.ScheduleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduleServiceImpl implements ScheduleService {
+
+    private ScheduleRepository scheduleRepository;
+
+    @Autowired
+    public ScheduleServiceImpl(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+    }
+
+    @Override
+    public Schedule createSchedule(Schedule schedule) {
+        scheduleRepository.save(schedule);
+        return schedule;
+    }
+
+    @Override
+    public Schedule getScheduleByID(Integer idSchedule) {
+        return scheduleRepository.findByIdSchedule(idSchedule);
+    }
+
+    @Override
+    public Schedule updateSchedule(Integer idSchedule, Schedule schedule) {
+        schedule.setIdSchedule(idSchedule);
+        scheduleRepository.save(schedule);
+        return schedule;
+    }
+
+    @Override
+    public void deleteSchedule(Integer idSchedule) {
+        Schedule schedule = scheduleRepository.findByIdSchedule(idSchedule);
+        scheduleRepository.delete(schedule);
+    }
+
+    @Override
+    public Iterable<Schedule> getScheduleByDay(String day) {
+        return scheduleRepository.findByDay(day);
+    }
+
+    @Override
+    public Iterable<Schedule> getListSchedule() {
+        return scheduleRepository.findAll();
+    }
+
+}
