7ebde86e44c32dc2997a14c297a18624cc0992dc
Budiman Arbenta <budiman.arbenta@ui.ac.id>
2021-05-21 18:07:52 +0700

    fixed bugs to pass unit test

--- src/main/java/com/adpro/remind/core/reminder/NotifyReminder.java
+++ src/main/java/com/adpro/remind/core/reminder/NotifyReminder.java
@@ -13,11 +13,11 @@ public class NotifyReminder {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
         Date now = new Date();
         String strDate = sdf.format(now);
-        Reminder[] reminders = getReminderFromDB();
-        for (Reminder reminder : reminders) {
-            if (strDate.equals(reminder.getDate())) {
-                // ToDo: send reminder to user then delete reminder from DB.
-            }
-        }
+//        Reminder[] reminders = getReminderFromDB();
+//        for (Reminder reminder : reminders) {
+//            if (strDate.equals(reminder.getDate())) {
+//                // ToDo: send reminder to user then delete reminder from DB.
+//            }
+//        }
     }
 }
