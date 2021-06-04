05ec02394866fd9e6a56e7b6d8b00d3a92c1371c
Budiman Arbenta <budiman.arbenta@ui.ac.id>
2021-06-04 19:59:37 +0700

    Selesai implementasi fitur notify reminder.

--- src/main/java/com/adpro/remind/command/help/HelpReminderCommand.java
+++ src/main/java/com/adpro/remind/command/help/HelpReminderCommand.java
@@ -21,7 +21,7 @@ public class HelpReminderCommand implements Command {
                 "Notes:\n" +
                 "1. Format TANGGAL: DD/MM/YYYY\n" +
                 "2. Format JAM: HH:MM\n" +
-                "3. Format WAKTU: X Hari atau X Jam\n" +
+                "3. Format WAKTU: X hari atau X jam\n" +
                 "4. Untuk melihat ID suatu Reminder, gunakan `-reminder show all` atau `-reminder show [TANGGAL]`\n");
 
         message.getChannel().sendMessage(eb.build()).queue();
