5db3ffdad05640ba95c3e0bc646b4ec827d9f819
Timothy Efraim Hotasi <timothyefraim35@gmail.com>
2021-06-09 19:47:06 +0700

    Delete unused code

--- src/main/java/com/adpro/remind/controller/FeatureCommand.java
+++ src/main/java/com/adpro/remind/controller/FeatureCommand.java
@@ -5,5 +5,4 @@ import net.dv8tion.jda.api.entities.Message;
 
 public interface FeatureCommand {
     void outputMessage(Message message, String[] inputContent);
-    //void outputPrivateMessage(Message message, String[] inputContent);
 }
