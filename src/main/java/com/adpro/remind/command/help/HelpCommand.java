b460729672db2b087529e1a80c39626ee86dd1cf
Budiman Arbenta <budiman.arbenta@ui.ac.id>
2021-06-10 19:43:49 +0700

    Fixed help command

--- src/main/java/com/adpro/remind/command/help/HelpCommand.java
+++ src/main/java/com/adpro/remind/command/help/HelpCommand.java
@@ -3,7 +3,6 @@ package com.adpro.remind.command.help;
 import com.adpro.remind.command.Command;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 
 public class HelpCommand implements Command {
     @Override
@@ -13,7 +12,7 @@ public class HelpCommand implements Command {
         eb.appendDescription("`-help` untuk menampilkan list ini\n" +
                 "`-help reminder` untuk menampilkan list command untuk fitur reminder\n" +
                 "`-help schedule` untuk menampilkan list command untuk fitur schedule\n" +
-                "`-help list` untuk menampilkan list command untuk fitur todolist\n");
+                "`-help list` untuk menampilkan list command untuk fitur todolist\n" +
                 "`-ping` untuk mengecek respond time\n");
 
         message.getChannel().sendMessage(eb.build()).queue();
